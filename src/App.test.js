b58816e9d188f71f37ab4269659bdfb2581b5f36
Ayush_sccg <ayush1240945@gmail.com>
2023-09-15 13:42:09 +0545

    wallaby

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import Enzymes , {shallow} from "enzyme"
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("addition" , ()=>{
+  const wrapper = shallow(<App/>)
+})
