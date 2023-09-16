393410f5a9170aa3992bd509d432c6acc79e23f2
Ayush_sccg <ayush1240945@gmail.com>
2023-09-16 15:22:23 +0545

    tdd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+  onInputChange(e) {
+    const val = e.target.value
+    const parts = val.match(/(\d+)([\+*])(\d+)/)
+    if (!parts) return
+    parts
+    const left = parseInt(parts[1]) // ?
+    const operator = parts[2] // ?
+    const right = parseInt(parts[3]) // ?
+    let  result
+    if (operator === '+') {
+      result = left + right
+    } else if (operator === '*') {
+      result = left * right
+    }
+    result
+    this.setState({ result })
+  }
+  render() {
+    return (
+      <div className="App">
+        <input onChange={this.onInputChange.bind(this)} />
+        <div id="result">{this.state.result}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
